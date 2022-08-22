86be11b137e9a49082e1acd9d04038827f2b8b7e
Erik DiMarco <erik.dimarco@dialexa.com>
2022-08-22 10:40:47 -0500

    Working authentication with user roles.

--- src/gql/users/resolver.ts
+++ src/gql/users/resolver.ts
@@ -1,11 +1,11 @@
 import { IContext } from 'gql/context';
-import { authentication } from 'middleware';
-import { Arg, Ctx, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { authentication, authorization } from 'middleware';
+import { Ctx, Query, Resolver, UseMiddleware } from 'type-graphql';
 import { User } from './schema';
 
 @Resolver(() => User)
 export class UsersResolver {
-	@UseMiddleware(authentication)
+	@UseMiddleware(authentication, authorization(['user']))
 	@Query(() => User, { nullable: true })
 	async me(@Ctx() { identity, db }: IContext): Promise<User> {
 		return (await db.users.findFirst({ where: { id: identity.id } })) as User;
